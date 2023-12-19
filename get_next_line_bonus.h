13fb92d0266c584ab65f083428d24faf4456d06c
lumikil <kilpelainenlumi@gmail.com>
2023-12-19 09:47:38 +0200

    update header file

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 09:33:34 by lkilpela          #+#    #+#             */
-/*   Updated: 2023/12/19 09:37:57 by lkilpela         ###   ########.fr       */
+/*   Updated: 2023/12/19 09:47:18 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,15 @@
 
 typedef struct s_list
 {
-	char	*buffer;
-	size_t	used;
-	size_t	size;
+	char    *buffer;
+    int     fd;
+    s_list    *next;
 }				t_list;
+
+char	*get_next_line(int fd);
+char	*ft_strchr(char *s, int c);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
 
 #endif
